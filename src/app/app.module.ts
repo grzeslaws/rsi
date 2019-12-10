8f6146a072861fdde513b462815dbd8a71d3302a
Grzegorz Supeł <grzesupel@gmail.com>
2019-12-10 10:48:53 +0100

    v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,31 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from "./components/home/home.component";
+import { TopBarComponent } from "./components/top-bar/top-bar.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SystemyComponent } from "./components/systemy/systemy.component";
+import { SystemComponent } from "./components/system/system.component";
+import { GestorzyComponent } from "./components/gestorzy/gestorzy.component";
+import { PaginationComponent } from "./components/pagination/pagination.component";
+import { ObiektyComponent } from "./components/obiekty/obiekty.component";
+import { CechyComponent } from "./components/cechy/cechy.component";
+import { SpinnerComponent } from "./components/spinner/spinner.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    // TopBarComponent,
-    // MenuComponent,
-    // FooterComponent,
+    TopBarComponent,
+    MenuComponent,
+    FooterComponent,
     HomeComponent,
-    // SystemyComponent,
-    // SystemComponent,
-    // GestorzyComponent,
-    // PaginationComponent,
-    // ObiektyComponent,
-    // CechyComponent,
-    // SpinnerComponent
+    SystemyComponent,
+    SystemComponent,
+    GestorzyComponent,
+    PaginationComponent,
+    ObiektyComponent,
+    CechyComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
