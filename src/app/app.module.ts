9c98c53e77cf485a154a72cdb093c69521eff358
Grzegorz Supeł <grzesupel@gmail.com>
2019-12-10 10:40:26 +0100

    v14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-import { TopBarComponent } from "./components/top-bar/top-bar.component";
-import { MenuComponent } from "./components/menu/menu.component";
-import { FooterComponent } from "./components/footer/footer.component";
 import { HomeComponent } from "./components/home/home.component";
-import { SystemyComponent } from "./components/systemy/systemy.component";
-import { SystemComponent } from "./components/system/system.component";
-import { GestorzyComponent } from "./components/gestorzy/gestorzy.component";
-import { PaginationComponent } from "./components/pagination/pagination.component";
-import { ObiektyComponent } from "./components/obiekty/obiekty.component";
-import { CechyComponent } from "./components/cechy/cechy.component";
-import { SpinnerComponent } from "./components/spinner/spinner.component";
-import { HomeRoutingModule } from "./home-routing.module";
 
 @NgModule({
   declarations: [
